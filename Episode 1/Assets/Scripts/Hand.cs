80685adfa855bc4ab1af2b319a3d2b522fe4478f
LochieWestfallGames <lochiewestfall@gmail.com>
2017-03-18 10:05:27 +1100

    Add files via upload

---
+++ Episode 1/Assets/Scripts/Hand.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller))]
+public class Hand : MonoBehaviour
+{
+    GameObject heldObject;
+    Controller controller;
+
+    Rigidbody simulator;
+
+    void Start()
+    {
+        simulator = new GameObject().AddComponent<Rigidbody>();
+        simulator.name = "simulator";
+        simulator.transform.parent = transform.parent;
+
+        controller = GetComponent<Controller>();
+    }
+
+    void Update()
+    {
+        if (heldObject)
+        {
+            simulator.velocity = (transform.position - simulator.position) * 50f;
+            if (controller.controller.GetPressUp(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
+            {
+                heldObject.transform.parent = null;
+                heldObject.GetComponent<Rigidbody>().isKinematic = false;
+                heldObject.GetComponent<Rigidbody>().velocity = simulator.velocity;
+                heldObject.GetComponent<HeldObject>().parent = null;
+                heldObject = null;
+            }
+        }
+        else
+        {
+            if (controller.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
+            {
+                Collider[] cols = Physics.OverlapSphere(transform.position, 0.1f);
+
+                foreach (Collider col in cols)
+                {
+                    if (heldObject == null && col.GetComponent<HeldObject>() && col.GetComponent<HeldObject>().parent == null)
+                    {
+                        heldObject = col.gameObject;
+                        heldObject.transform.parent = transform;
+                        heldObject.transform.localPosition = Vector3.zero;
+                        heldObject.transform.localRotation = Quaternion.identity;
+                        heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                        heldObject.GetComponent<HeldObject>().parent = controller;
+                    }
+                }
+            }
+        }
+    }
+}
