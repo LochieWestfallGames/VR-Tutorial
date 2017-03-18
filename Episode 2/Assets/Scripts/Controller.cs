39cf11767dde7f6e0d677ea56d92652ff646b2a3
LochieWestfallGames <lochiewestfall@gmail.com>
2017-03-18 21:08:38 +1100

    Add files via upload

---
+++ Episode 2/Assets/Scripts/Controller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public SteamVR_Controller.Device controller
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)GetComponent<SteamVR_TrackedObject>().index);
+        }
+    }
+}
---
+++ Episode 2/Assets/Scripts/Hand.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller))]
+public class Hand : MonoBehaviour
+{
+    GameObject heldObject;
+    Controller controller;
+
+    Rigidbody simulator;
+
+    public Valve.VR.EVRButtonId pickUpButton;
+    public Valve.VR.EVRButtonId dropButton;
+
+    void Start()
+    {
+        simulator = new GameObject().AddComponent<Rigidbody>();
+        simulator.name = "simulator";
+        simulator.transform.parent = transform.parent;
+
+        controller = GetComponent<Controller>();
+    }
+
+    void Update()
+    {
+        if (heldObject)
+        {
+            simulator.velocity = (transform.position - simulator.position) * 50f;
+            if ((controller.controller.GetPressUp(pickUpButton) && heldObject.GetComponent<HeldObject>().dropOnRelease) || (controller.controller.GetPressDown(dropButton) && !heldObject.GetComponent<HeldObject>().dropOnRelease))
+            {
+                heldObject.transform.parent = null;
+                heldObject.GetComponent<Rigidbody>().isKinematic = false;
+                heldObject.GetComponent<Rigidbody>().velocity = simulator.velocity;
+                heldObject.GetComponent<HeldObject>().parent = null;
+                heldObject = null;
+            }
+        }
+        else
+        {
+            if (controller.controller.GetPressDown(pickUpButton))
+            {
+                Collider[] cols = Physics.OverlapSphere(transform.position, 0.1f);
+
+                foreach (Collider col in cols)
+                {
+                    if (heldObject == null && col.GetComponent<HeldObject>() && col.GetComponent<HeldObject>().parent == null)
+                    {
+                        heldObject = col.gameObject;
+                        heldObject.transform.parent = transform;
+                        heldObject.transform.localPosition = Vector3.zero;
+                        heldObject.transform.localRotation = Quaternion.identity;
+                        heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                        heldObject.GetComponent<HeldObject>().parent = controller;
+                    }
+                }
+            }
+        }
+    }
+}
